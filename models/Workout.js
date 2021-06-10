3bf856c8436c06f07fd3677a10f559cd09020d21
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 00:44:28 -0500

    Added to models.

---
+++ models/Workout.js
@@ -0,0 +1,14 @@
+//Require Mongoose
+
+
+
+//Needed for model
+// workoutData.type = "resistance";
+// workoutData.name = nameInput.value.trim();
+// workoutData.weight = Number(weightInput.value.trim());
+// workoutData.sets = Number(setsInput.value.trim());
+// workoutData.reps = Number(repsInput.value.trim());
+// workoutData.duration = Number(resistanceDurationInput.value.trim());
+
+
+//export model
