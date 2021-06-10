59e08aec139a0ce7643d69b983320efca0447337
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 00:38:24 -0500

    Added all html routes to the hmtlRoutes.js.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,20 @@
 //Require for router
 const router = require("express").Router();
-
+const path = require("path");
 
 //Get all html pages
 
 router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, ''));
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, ''));
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
 
 //Landing page 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, ''));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
