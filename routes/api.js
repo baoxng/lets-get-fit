b76931db0c436c782672ce7f85f428342a7336c5
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 13:18:04 -0500

    Trouble shooting.

--- routes/api.js
+++ routes/api.js
@@ -35,23 +35,20 @@ router.post("/api/workouts", ({ body }, res) => {
 
 
 //Adding exercise
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-    Workout.findByIdAndUpdate(
-        params.id,
-        {
-            $push: {exercises: body} 
-        },
-        {
-            new: true,
-            runValidators: true
-        }
-    )
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-    });
+router.put("/api/workouts/:id", (req, res) => {
+
+    Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+          $inc: { totalDuration: req.body.duration },
+          $push: { exercises: req.body }
+      },
+      { new: true }).then(dbWorkout => {
+          res.json(dbWorkout);
+      }).catch(err => {
+          res.json(err);
+      });
+
 });
 
 
