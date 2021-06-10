9779d45aaf36739c1f60e9317fcb156d493488f9
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 01:31:39 -0500

    Added Workout to models.

--- models/Workout.js
+++ models/Workout.js
@@ -1,14 +1,47 @@
-//Require Mongoose
-
+//Require Mongoose 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 
 //Needed for model
-// workoutData.type = "resistance";
-// workoutData.name = nameInput.value.trim();
-// workoutData.weight = Number(weightInput.value.trim());
-// workoutData.sets = Number(setsInput.value.trim());
-// workoutData.reps = Number(repsInput.value.trim());
-// workoutData.duration = Number(resistanceDurationInput.value.trim());
 
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date(),
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "enter an exercise"
+            },
+            name: {
+                type: String,
+                required: "enter the name of the exercise"
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number,
+                required: "enter length of exercise"
+            },
+            distance: {
+                type: Number
+            }
+
+        }
+    ]
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
 
 //export model
