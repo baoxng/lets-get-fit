7aead600f04959ede3cddfbba1d7733a698f6040
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 01:39:09 -0500

    Added get route to api.

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,20 @@
 const router = require('express').Router();
 
 //Need model for workouts
-
+const Workout = require("../models/Workout");
 
 //Getting workouts
-
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 //Posting workouts
-
-
-//Updating workouts
 
 
 //Getting workouts in range
